fb8303ad425a6fa1e0938ccc471e592ee314790c
sujoncoder <sujonsheikh.in@gmail.com>
2025-03-15 22:20:57 +0600

    understanding confusing this

--- advance/this.js
+++ advance/this.js
@@ -1,20 +1,47 @@
 // NOTE : THIS NODE ENVIRONMENT RETURN {} OBJECT. AND BROWSER ENVIRONMENT RETURN WINDOW OBJECT.
-console.log(this);
 
-function one() {
-    const name = "sujon"
-    console.log(this)
-};
-one(); // undefined
 
-const two = function () {
-    const name = "sujon2"
-    console.log(this)
-};
-two(); // undefined
+// ARROW FUNCTION THIS MEAN WINDOW OR GLOBAL THIS
+const person = {
+    name: "Sheikh",
+    age: 25,
+    arrowFunc: () => {
+        console.log("ths", this)
+    }
+}
+person.arrowFunc();
 
-const arrowFunc = () => {
-    const name = "Sujon Sheikh"
-    console.log(this)
+// ------------------OBJECT IN THIS------------------
+const player = {
+    name: "sujon",
+    country: "Bangladeshi",
+    run: 2800,
+    showRun() {
+        return this
+        // return this.run
+    }
 };
-arrowFunc();
+console.log(player.showRun());
+
+// --------THIS HAVE DIFFERNT FACE WHERE WHY HOW TO DEFINED YOU THIS --------
+// student object
+const student = {
+    name: "Learner",
+    getThis() {
+        console.log(this)
+    }
+};
+student.getThis();
+
+// teacher object
+const teacher = {
+    name: "Teacher"
+};
+teacher.getThis = student.getThis;
+teacher.getThis();
+
+// free this
+const freeThis = student.getThis;
+console.log(freeThis);
+
+// NOTE : ALWAYS LOOKS BEFORE DOT IF SEE ANYTHING THAT,S MEAN DENOTE IT IF HAVE NOT ANYTHING MEANS IT DENOTE WINDOW OR GLOBAL. 
