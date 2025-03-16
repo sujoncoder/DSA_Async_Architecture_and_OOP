aae40175e169c0b363dff95872039fb075a4d3ea
sujoncoder <sujonsheikh.in@gmail.com>
2025-03-16 08:47:51 +0600

    again touch

--- advance/this-bind-call-apply.js
+++ advance/this-bind-call-apply.js
@@ -1,3 +1,5 @@
+// NOTE : CALL, BIND, APPLY MAIN CONTEXT IS SHARE ACCROSS THIS CONTEX
+
 const sujonInfo = {
     name: "Sujon",
     age: 25,
@@ -28,3 +30,49 @@ const nuraiya = {
     age: 18
 };
 sujonInfo.getInfo.apply(nuraiya);
+
+
+// ---------------------------------------------------- //
+
+// CALL --- The call() method takes arguments separately.
+const person = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+}
+
+const person1 = {
+    firstName: "Sujon",
+    lastName: "Sheikh"
+};
+console.log(person.fullName.call(person1, "Magura", "Bangladesh"));
+
+// APPLY -- The apply() method takes arguments as an array.
+const anotherPerson = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+};
+
+const anotherPerson1 = {
+    firstName: "Jerin",
+    lastName: "Jaman"
+};
+console.log(anotherPerson.fullName.apply(anotherPerson1, ["Gazipur", "Bangladesh"]));
+
+// BIND
+const nextPerson = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const member = {
+    firstName: "Nuraiya",
+    lastName: "Sheikh",
+}
+
+let fullName = nextPerson.fullName.bind(member);
+console.log(fullName());
